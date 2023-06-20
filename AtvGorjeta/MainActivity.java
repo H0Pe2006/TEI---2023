6c4d33fc3b4e371d120f679130272b7c2d9691db
Luiz Felipe <124799524+Leuis0101@users.noreply.github.com>
2023-06-20 16:14:46 -0300

    Update MainActivity.java

--- AtvGorjeta/MainActivity.java
+++ AtvGorjeta/MainActivity.java
@@ -1,14 +1,24 @@
-package com.example.atividade_gorjeta;
+package com.example.atv_gorjeta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekbarPessoas;
+    private SeekBar seekbarGorjeta;
+    private EditText ValorConta;
+    private TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void
 }
