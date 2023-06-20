d03309e8ad74b2b01b4285ec5c988870cf147ff1
Luiz Felipe <124799524+Leuis0101@users.noreply.github.com>
2023-06-20 17:01:41 -0300

    Update MainActivity.java

--- AtvGorjeta/MainActivity.java
+++ AtvGorjeta/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.atv_gorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -13,12 +14,38 @@ public class MainActivity extends AppCompatActivity {
     private SeekBar seekbarGorjeta;
     private EditText ValorConta;
     private TextView textResultado;
+    private EditText editTextValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekbarPessoas.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textResultado.setText("Pessoas: "+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                textResultado.setText("Pessoas: "+seekBar.getProgress());
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textResultado.setText("Pessoas: "+seekBar.getProgress());
+            }
+        });
     }
 
-    public void
+
+
+    public void CalcularConta(View view){
+        textResultado = findViewById((R.id.textResultado));
+
+        textResultado.setText("");
+
+//        textResultado.setText("Valor sem Gooerjeta")
+    }
 }
