8d37019f81ca12f4c61462bf16ed38874662056f
Luiz Felipe <124799524+Leuis0101@users.noreply.github.com>
2023-07-05 13:50:57 -0300

    Update MainActivity.java

--- AtvGorjeta/MainActivity.java
+++ AtvGorjeta/MainActivity.java
@@ -1,51 +1,97 @@
 package com.example.atv_gorjeta;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.atv_gorjeta.R;
 
 public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBarPessoas;
 
-    private SeekBar seekbarPessoas;
-    private SeekBar seekbarGorjeta;
-    private EditText ValorConta;
-    private TextView textResultado;
-    private EditText editTextValor;
+    private SeekBar seekBarGorjeta;
+    private TextView pessoas;
 
+    private TextView gorjet;
+    private EditText valorConta;
+    private int numPessoas;
+    private int valorGorjeta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        seekbarPessoas.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        pessoas = findViewById(R.id.numPessoas);
+        gorjet = findViewById(R.id.valorGorjeta);
+        seekBarPessoas = findViewById(R.id.seekBarPessoas);
+        seekBarPessoas.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textResultado.setText("Pessoas: "+progress);
+                numPessoas = progress;
+                pessoas.setText("Números de Pessoas: " + numPessoas);
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                textResultado.setText("Pessoas: "+seekBar.getProgress());
+                numPessoas = seekBar.getProgress();
+                pessoas.setText("Números de Pessoas: " + numPessoas);
             }
-
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                textResultado.setText("Pessoas: "+seekBar.getProgress());
+                numPessoas = seekBar.getProgress();
+                pessoas.setText("Números de Pessoas: " + numPessoas);
+            }
+        });
+        seekBarGorjeta = findViewById(R.id.seekBarGorjeta);
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                valorGorjeta = progress;
+                gorjet.setText("Porcentagem da Gorjeta: " + valorGorjeta);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                valorGorjeta = seekBar.getProgress();
+                gorjet.setText("Porcentagem da Gorjeta: " + valorGorjeta);
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                valorGorjeta = seekBar.getProgress();
+                gorjet.setText("Porcentagem da Gorjeta: " + valorGorjeta);
             }
         });
     }
+    public void calcValor(View view){
+        valorConta = findViewById(R.id.valorConta);
+        double valorgorjet = valorGorjeta;
+        double valor = Double.parseDouble(valorConta.getText().toString());
+        double valorIndividual = valor/numPessoas;
+        double indivGorj = valorIndividual+(valorIndividual*(valorgorjet/100));
+        AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+        alerta.setTitle("Gorjeta: ");
+        alerta.setMessage("Aperte 'CG' para ver o valor individual com a gorjeta e 'SG' se quiser ver com a gorjeta");
+        alerta.setPositiveButton("CG", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        String.format("Valor individual com a gorjeta: R$%.2f",indivGorj),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        alerta.setNegativeButton("SG", new DialogInterface.OnClickListener() {
 
-
-
-    public void CalcularConta(View view){
-        textResultado = findViewById((R.id.textResultado));
-
-        textResultado.setText("");
-
-//        textResultado.setText("Valor sem Gooerjeta")
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),
+                        String.format("Valor individual sem gorjeta: R$%.2f",valorIndividual),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        alerta.create();
+        alerta.show();
     }
 }
